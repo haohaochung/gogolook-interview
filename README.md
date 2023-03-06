433d5d67f777a090dde26efde36e73bd9cf449ce
haohaochung <colin.chung@coolbitx.com>
2023-03-06 23:21:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# gogolook-interview
+
+## Set Up
+   1. pip3 install -r requirements.txt
+   2. copy .env.template and rename as .env
+   3. input your 104 account data in .env 
+## Run code
+   1. python3 main.py
---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import os
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set up .env and selenium driver 
+load_dotenv()
+driver = webdriver.Chrome()
+url = ("https://www.104.com.tw/jobs/main/")
+driver.get(url)
+
+# main page (without login)
+driver.find_element(By.XPATH, '//*[@id="global_bk"]/ul/li[2]/ul/li[6]/a').click()
+
+# login page
+driver.find_element(By.ID, "username").send_keys(os.getenv('ACCOUNT'))
+driver.find_element(By.ID, "password").send_keys(os.getenv('PASSWORD'))
+driver.find_element(By.ID, "submitBtn").click()
+
+# main page (with login)
+WebDriverWait(driver=driver, timeout=10).until(EC.visibility_of_element_located((By.ID, "myName")))
+driver.find_element(By.ID, "myName").click()
+driver.find_element(By.XPATH, '//*[@id="global_bk"]/ul/li[2]/ul/li[4]/ul/li/div/dl/dt[1]/a').click()
+
+# member center page
+time.sleep(5)
+driver.find_element(By.XPATH, '//*[@id="global_bk"]/ul/li[2]/ul/li[5]/a').click()
+
